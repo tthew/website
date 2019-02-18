53f46e050549383526e7602c2223cb7e7c266783
Matt Richards <matt@lucidmoon.co.uk>
2019-02-18 08:25:54 +0100

    chore: pages

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Hero from '../components/hero'
+import About from '../components/about'
+import Experience from '../components/experience'
+
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title='Freelance Web Development' keywords={[`freelance`, `web developer`, `berlin`]} />
+      <Hero />
+      <About />
+      <Experience />
+    </Layout>
+  )
+}
+
+export default IndexPage
