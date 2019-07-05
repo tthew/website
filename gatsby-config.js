0870d6af6b923804df5a3e12517f13511a987315
Matt Richards <m@tthew.berlin>
2019-07-05 16:48:09 +0200

    styled-components gatsby integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,12 @@ module.exports = {
         `
       }
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     "gatsby-plugin-offline"
