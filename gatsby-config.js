4b9791aa1019631172ebfb7b753cf541f0f40a4e
Matt Richards <matt@lucidmoon.co.uk>
2021-09-15 11:20:35 +0200

    fix: google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts-v2`,
       options: {
         fonts: [
           {
