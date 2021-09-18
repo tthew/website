bda6a15ebfd0b50e45c195d392a10222627db0ba
Matt Richards <matt@lucidmoon.co.uk>
2021-09-18 13:33:44 +0200

    fix: build, fonts & CSP
    * chore: remove package-lock.json
    
    * chore: .node-version
    
    * chore: csp
    
    * chore: csp again
    
    * chore: csp
    
    * fix: warning
    
    * fix: csp
    
    * fix: csp
    
    * fix: csp omg

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,8 +1,7 @@
+import { graphql } from 'gatsby'
 import React from 'react'
-
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-
 import pageStyles from './page.module.css'
 
 const Page = ({ data }) => (
