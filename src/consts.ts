899abead58ec27e42906e0cd67542e129cb4779b
Matthew Richards <matthew.richards.ext@doctorly.de>
2023-10-24 21:20:38 +0200

    fix: further design iteration

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Lead Frontend Developer Berlin";
+export const SITE_TITLE = "Principle Frontend Engineer Berlin";
 export const SITE_DESCRIPTION =
-  "👋 Hi, I'm Matt. A Lead Frontend Developer & Staff Engineer based in Berlin.\nI've been building, designing & leading teams that build for the web for over 20 years.";
+  "👋 Hi, I'm Matt. A Lead Frontend Engineer based in Berlin 🇩🇪.\n\nI've been 🔧 building, 🎨 designing & 🚀 leading teams that build for the web, for over 20 years";
