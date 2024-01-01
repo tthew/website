a8f7932810e9036d2c2097097e3980b98a045c5b
Matthew Richards <matthew.richards.ext@doctorly.de>
2024-01-01 23:05:45 +0100

    ♻️ clean

--- src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Principle Frontend Engineer Berlin";
-export const SITE_DESCRIPTION =
-  "👋 Hi, I'm Matt. A Lead Frontend Engineer based in Berlin 🇩🇪.\n\nI've been 🔧 building, 🎨 designing & 🚀 leading teams that build for the web, for over 20 years";
