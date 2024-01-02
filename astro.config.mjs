4917ebe5affdeb779da58e549c402b4e2ba32b16
Matthew Richards <matthew.richards.ext@doctorly.de>
2024-01-02 21:08:25 +0100

    ✨ tweak design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://ma.tthew.berlin",
   integrations: [
     mdx(),
     sitemap(),
